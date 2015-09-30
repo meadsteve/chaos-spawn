f92e524839bfd8a4b2528da7faec6d2edbffe84e
steve.brazier <steve.brazier@just-eat.com>
2015-09-30 11:52:16 +0100

    fix example test name

--- test/chaos_spawn/examples/gen_server_test.exs
+++ test/chaos_spawn/examples/gen_server_test.exs
@@ -1,7 +1,7 @@
 defmodule Example.GenServerTest do
   use ExUnit.Case
 
-  test "can kill based on pid" do
+  test "the gen server provide example should work" do
     {:ok, server} = ChaosSpawn.Example.GenServer.start_link
     assert ChaosSpawn.Example.GenServer.say_hello(server) == "hello"
   end
