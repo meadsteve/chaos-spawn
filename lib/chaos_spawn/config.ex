1562c3a9f433c37101f72d6917b9c97cc75caadf
steve.brazier <steve.brazier@just-eat.com>
2015-10-13 16:52:37 +0100

    move more config in to module

--- lib/chaos_spawn/config.ex
+++ lib/chaos_spawn/config.ex
@@ -3,15 +3,22 @@ defmodule ChaosSpawn.Config do
   Wraps around config keys used by ChaosSpawn
   """
 
+  @default_kill_tick 1000  # 1 second
+  @default_kill_prob 0.015 # Kill about 1 process a minute
+  @defualt_kill_window :anytime
+
   def kill_config do
-    case get_setting(:only_kill_between) do
-      nil         -> []
+    case get_setting(:only_kill_between, @defualt_kill_window) do
+      :anytime    -> []
       time_window -> [{:only_kill_between, time_window}]
     end
   end
 
-  defp get_setting(setting) do
-    Application.get_env(:chaos_spawn, setting, nil)
+  def kill_tick, do: get_setting(:kill_tick,        @default_kill_tick)
+  def kill_prob, do: get_setting(:kill_probability, @default_kill_prob)
+
+  defp get_setting(setting, default) do
+    Application.get_env(:chaos_spawn, setting, default)
   end
 
 end
