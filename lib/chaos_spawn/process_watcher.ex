507b746115d15e13e5c250d211b5e50cdd48e04f
steve.brazier <steve.brazier@just-eat.com>
2015-10-23 14:34:11 +0100

    fix unused alias warning

--- lib/chaos_spawn/process_watcher.ex
+++ lib/chaos_spawn/process_watcher.ex
@@ -5,15 +5,14 @@ defmodule ChaosSpawn.ProcessWatcher do
   """
   use ExActor.GenServer
   require Logger
-  alias ChaosSpawn.PidList
 
   defstart start_link, gen_server_opts: :runtime, do: initial_state([])
 
   defcall all_pids, state: pids, do: reply(pids)
 
   defcall get_random_pid, state: pids do
-    updated_pids = pids |> PidList.only_alive
-    pid = PidList.pick_random(updated_pids)
+    updated_pids = pids |> ChaosSpawn.PidList.only_alive
+    pid = ChaosSpawn.PidList.pick_random(updated_pids)
 
     set_and_reply(updated_pids, pid)
   end
@@ -32,7 +31,7 @@ defmodule ChaosSpawn.ProcessWatcher do
   end
 
   defcast tidy_pids(), state: pids do
-    new_state(pids |> PidList.only_alive)
+    new_state(pids |> ChaosSpawn.PidList.only_alive)
   end
 
 end
