8855a5666879db9a760053cddafb1ed363f95fca
steve.brazier <meadsteve@gmail.com>
2015-10-31 11:46:27 +0000

    add additional test to worker wrapper

--- test/chaos_spawn/chaotic/worker_test.exs
+++ test/chaos_spawn/chaotic/worker_test.exs
@@ -1,6 +1,7 @@
 defmodule Chaotic.WorkerTest do
   use ExUnit.Case
   alias ChaosSpawn.Chaotic.ChaoticWorker
+  alias Chaotic.WorkerTest.Example
 
   test "worker/2 wraps up a call to Supervisor.Spec worker" do
     args = [:arg_one, :arg_two]
@@ -34,4 +35,18 @@ defmodule Chaotic.WorkerTest do
     assert worker == expected
   end
 
+  test "start_link_wrapper/3 calls the wrapped start_func" do
+    args = [:expected_arg]
+    {:ok, pid} = ChaoticWorker.start_link_wrapper(Example, :start_link, args)
+    assert is_pid(pid)
+  end
+end
+
+defmodule Chaotic.WorkerTest.Example do
+  def start_link(:expected_arg) do
+    {:ok, spawn fn -> receive do
+            _ -> :ok
+          end end
+    }
+  end
 end
