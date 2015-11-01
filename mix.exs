5c65cbbbd29a4d3fb74db402d55cd6e752c9f8dd
steve.brazier <meadsteve@gmail.com>
2015-11-01 09:11:18 +0000

    prepare next version number

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule ChaosSpawn.Mixfile do
        licenses: ["MIT"],
        links: %{"GitHub" => "https://github.com/meadsteve/chaos-spawn"},
      ],
-     version: "0.3.0",
+     version: "0.4.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
