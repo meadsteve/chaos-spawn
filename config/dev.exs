a91d51202b548fc58490986e16ea4c18520f63fe
steve.brazier <steve.brazier@just-eat.com>
2015-12-02 11:47:49 +0000

    add allowed killing day config

--- config/dev.exs
+++ config/dev.exs
@@ -5,6 +5,7 @@ config :chaos_spawn, :fake_fixed_now, true
 #config :chaos_spawn, :kill_tick, 1000
 #config :chaos_spawn, :kill_probability, 0.1
 #config :chaos_spawn, :only_kill_between, {{10, 00, 00}, {16, 00, 00}}
+#config :chaos_spawn, :only_kill_on_days, [:mon, :tue, :wed, :thu, :fri]
 # config :chaos_spawn, :skip_worker_modules, [
 #   Chaotic.WorkerTest.Example
 # ]
--- lib/chaos_spawn/config.ex
+++ lib/chaos_spawn/config.ex
@@ -6,13 +6,13 @@ defmodule ChaosSpawn.Config do
   @default_kill_tick 1000  # 1 second
   @default_kill_prob 0.015 # Kill about 1 process a minute
   @default_kill_window :anytime
+  @default_kill_days :anyday
   @default_skipped_workers [] # Nothing skipped by default
 
   def kill_config do
-    case get_setting(:only_kill_between, @default_kill_window) do
-      :anytime    -> []
-      time_window -> [{:only_kill_between, time_window}]
-    end
+    []
+      |> add_kill_window
+      |> add_allowed_killing_days
   end
 
   def kill_tick, do: get_setting(:kill_tick,        @default_kill_tick)
@@ -24,6 +24,20 @@ defmodule ChaosSpawn.Config do
 
   defp get_setting(setting, default) do
     Application.get_env(:chaos_spawn, setting, default)
+  end
+
+  defp add_kill_window(config) do
+    case get_setting(:only_kill_between, @default_kill_window) do
+      :anytime    -> config
+      time_window -> [{:only_kill_between, time_window} | config]
+    end
+  end
+
+  defp add_allowed_killing_days(config) do
+    case get_setting(:only_kill_on_days, @default_kill_days) do
+      :anyday  -> config
+      days     -> [{:only_kill_on_days, days} | config]
+    end
   end
 
 end
